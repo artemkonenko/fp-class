8af92f1069f8dad158e64a1154aec7220e555bc6
Konenko Artem <yadummer@gmail.com>
2014-09-11 23:51:55 +0400

    Class-02 complete up to 2.8

--- class-02.hs
+++ class-02.hs
@@ -2,14 +2,18 @@
 -- Написать функцию, которая разбивает промежуток времени в секундах на часы, минуты и секунды.
 -- Результат возвращать в виде кортежа из трёх элементов. Реализовать также обратное преобразование.
 sec2hms :: Int -> (Int, Int, Int)
-sec2hms = undefined
+sec2hms sec = ( h, m, s )
+			where 
+				h = div sec 3600
+				m = flip div 60 $ mod sec 3600
+				s = mod sec 60
 
 hms2sec :: (Int, Int, Int) -> Int
-hms2sec (h, m, s) = undefined
+hms2sec (h, m, s) = h * 3600 + m * 60 + s
 
 -- Реализовать с помощью hms2sec (здесь параметры заданы по отдельности)
 hms2sec' :: Int -> Int -> Int -> Int
-hms2sec' = undefined
+hms2sec' h m s = hms2sec (h, m, s)
 
 -- должно быть True
 test1 = and $ map (\x -> x == hms2sec (sec2hms x)) [1,10..10000]
@@ -22,13 +26,20 @@ test1 = and $ map (\x -> x == hms2sec (sec2hms x)) [1,10..10000]
 type Point = (Double, Double)
 
 length :: Point -> Point -> Double
-length (x1, y1) (x2, y2) = undefined
-
--- triangle :: ??? -> (Double, Double)
-triangle _ = (p, s)
+length (x1, y1) (x2, y2) = let
+							a = flip (^) 2 $ (-) x1 x2
+							b = flip (^) 2 $ (-) y1 y2
+						   in sqrt $ (+) a b
+						
+-- triangle :: Point -> Point -> Point -> (Double, Double)
+triangle a b c = (p, s)
   where
-    p = undefined
-    s = undefined
+	la = Main.length a b
+	lb = Main.length a c
+	lc = Main.length b c
+	p  = la + lb + lc
+	halfp = p / 2
+	s = sqrt $ halfp * (halfp - la) * (halfp - lb) * (halfp - lc)
 
 -- Во всех следующих заданиях использование стандартных функций обработки списков не допускается.
 -- Все решения должны реализовываться рекурсивными функциями.
@@ -37,7 +48,11 @@ triangle _ = (p, s)
 -- Определить рекурсивную функцию, определяющую количество чётных элементов списка
 nEven :: Integral a => [a] -> Int
 nEven [] = 0
-nEven (x:xs) = undefined
+nEven (x:xs) = (+) cur $ nEven xs 
+		where
+			cur
+				| even x = 1
+				| otherwise = 0
 
 -- 2.2
 -- Увеличить все элементы заданного списка в два раза.
@@ -45,22 +60,47 @@ nEven (x:xs) = undefined
 -- > 1 : [2,3,4]
 --   [1,2,3,4]
 doubleElems :: Num a => [a] -> [a]
-doubleElems = undefined
+doubleElems [] = []
+doubleElems (x:xs) = x * 2 : doubleElems xs
 
 
 -- 2.3
 -- Дан список целых чисел. Сформировать новый список, содержащий только нечетные элементы исходного.
 fltOdd :: Integral a => [a] -> [a]
 fltOdd [] = []
-fltOdd (x:xs) = undefined
+fltOdd (x:xs) = cur ++ fltOdd xs
+	where
+		cur
+			| odd x = [x]
+			| otherwise = []
 
 
 -- 2.4
 -- Написать следующие функции обработки списков:
 -- а) удалить все отрицательные элементы;
+fltRmNeg :: Integral a => [a] -> [a]
+fltRmNeg [] = []
+fltRmNeg (x:xs) = cur ++ fltRmNeg xs
+	where
+		cur
+			| x >= 0 = [x]
+			| otherwise = []
+
 -- б) увеличить элементы с чётными значениями в два раза;
+rltTwiceEven :: Integral a => [a] -> [a]
+rltTwiceEven [] = []
+rltTwiceEven (x:xs) = cur ++ rltTwiceEven xs
+	where
+		cur
+			| x >= 0 = [x]
+			| otherwise = []
+
 -- в) переставить местами чётные и нечётные по порядку следования элементы
 --    (для списков нечётной длины отбрасывать последний элемент).
+fltSwapPairs :: Integral a => [a] -> [a]
+fltSwapPairs [] = []
+fltSwapPairs (x1:x2:xs) = [x2] ++ [x1] ++ fltSwapPairs xs
+fltSwapPairs (x1) = []
 
 -- 2.5 
 -- Даны два списка целых чисел. Сформировать список, каждый элемент которого равен сумме
@@ -68,19 +108,32 @@ fltOdd (x:xs) = undefined
 combine_plus :: [Integer] -> [Integer] -> [Integer]
 combine_plus [] ys = ys
 combine_plus xs [] = xs
-combine_plus (x:xs) (y:ys) = undefined
+combine_plus (x:xs) (y:ys) = x + y : combine_plus xs ys
 
 -- 2.6
 -- Даны два списка. Сформировать новый список, содержащий пары из соответствующих элементов
 -- исходных списков. Хвост более длинного списка отбросить.
+combine_pair :: [Integer] -> [Integer] -> [(Integer, Integer)]
+combine_pair [] ys = []
+combine_pair xs [] = []
+combine_pair (x:xs) (y:ys) = (x, y) : combine_pair xs ys
 
 -- 2.7
 -- Написать функции, которые по заданному n возвращают список, состоящий из n первых натуральных чисел
 -- а) в порядке убывания;
+getReverseFirstN :: Integer -> [Integer]
+getReverseFirstN 0 = []
+getReverseFirstN n = n : getReverseFirstN (n - 1)
+
 -- б) в порядке возрастания.
+getFirstN :: Integer -> [Integer]
+getFirstN n = reverse $ getReverseFirstN n
 
 -- 2.8
 -- Дан элемент типа a и список [a]. Вставить между всеми элементами списка заданный элемент.
+spamAinList :: a -> [a] -> [a]
+spamAinList y (x:xs) = x : y : spamAinList y xs
+spamAinList y x = x
 
 -- 2.9
 -- Написать функцию, которая разбивает список на два подсписка: элементы из начала списка,
