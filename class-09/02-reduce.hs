9df83d142748eced74f38bdcd4397904128937c4
Vitaly Bragilevsky <bravit111@gmail.com>
2014-10-28 12:55:40 +0300

    Добавлено девятое задание

---
+++ class-09/02-reduce.hs
@@ -0,0 +1,63 @@
+import System.Environment
+
+{-
+  Напишите функцию reduce, принимающую один целочисленный аргумент a и возвращающую 0,
+  если аргумент делится на 3, a^2, если он на 3 не делится и является при этом нечётным,
+  a^3 в остальных случаях.
+-}
+
+reduce :: Integral a => a -> a
+reduce = undefined
+
+{-
+  Напишите функцию, применяющую функцию reduce заданное количество раз к значению в контексте,
+  являющемся функтором:
+-}
+
+reduceNF :: (Functor f, Integral a) => Int -> f a -> f a
+reduceNF = undefined
+
+{-
+  Реализуйте следующие функции-преобразователи произвольным, но, желательно, осмысленным и
+  нетривиальным способом.
+-}
+
+toList :: Integral a => [(a, a)]  -> [a]
+toList = undefined
+
+toMaybe :: Integral a => [(a, a)]  -> Maybe a
+toMaybe = undefined
+
+toEither :: Integral a => [(a, a)]  -> Either String a
+toEither = undefined
+
+-- воспользуйтесь в этой функции случайными числами
+toIO :: Integral a => [(a, a)]  -> IO a
+toIO = undefined
+
+{-
+  В параметрах командной строки задано имя текстового файла, в каждой строке
+  которого записана пара целых чисел, разделённых пробелами. Загрузите
+  данные из файла в список пар целых чисел, преобразуйте этот список к
+  значениям в контекстах [], Maybe, Either String и IO и примените к каждому
+  из контекстов функцию reduceNF (значение N также должно браться из 
+  параметров командной строки).
+-}
+
+parseArgs :: [String] -> (FilePath, Int)
+parseArgs = undefined
+
+readData :: FilePath -> IO [(Int, Int)]
+readData = undefined
+
+main = do
+  (fname, n) <- parseArgs `fmap` getArgs
+  ps <- readData fname
+  undefined
+  print $ reduceNF n (toEither ps)
+  reduceNF n (toIO ps) >>= print
+
+{-
+  Подготовьте несколько тестовых файлов, демонстрирующих особенности различных контекстов.
+  Скопируйте сюда результаты вызова программы на этих файлах.
+-}
