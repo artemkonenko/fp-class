3f8a1c30dae4717126cdf65d5c24e0af90a96b52
Artem Konenko <yadummer@gmail.com>
2014-11-04 04:01:36 +0300

    remove unnecessary file

--- class-08/03-queues/mtest.hs
+++
@@ -1,9 +0,0 @@
-import System.Environment
-import System.Random
-import Control.Monad
-import AbstractQueue
-import qualified Queue as Q
-import qualified FastQueue as FQ
-
-main = do
-        getArgs >>= return.head >>= putStrLn >> return ()
